b3a5e75a5abf215ba596d9925cbc03f6855a0628
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-04-03 00:04:31 +0530

    re-rendered the data

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,12 +1,19 @@
 import React from "react";
 
+import { Component } from 'react'
 
-export default class Hello extends React.Component {
-  render() {
+export default class Hello extends Component {
+    data = { first: 'ankur', second : 'wadia'}
+    c = "John" 
+    render() {
+    
     return (
       <div>
+        <h1>{this.c}</h1>
+        <h1>{this.data.first}</h1>
         Hello World!
       </div>
     )
   }
 }
+
