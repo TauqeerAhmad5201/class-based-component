5a394977120920cb858d767f87123d7bbccf5ccb
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-04-04 23:41:02 +0530

    attached specific

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Hello from './components/Hello';
+import Specific from './components/Specific'
 
 function App() {
   return (
     <>
     <Hello></Hello>
-    
+    <Specific></Specific>
     </>
   );
 }
