548a4c4132ef33d3a6cfd7ad47b8a51d29972706
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-04-07 23:55:20 +0530

    set the this.state

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Data extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       data: 'Content to be published'
+    }
+  }
+  
   render() {
     return (
       <div>
